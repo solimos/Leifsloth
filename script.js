c02375f00c5c94725187f0837f1b27204065f53a
solimos <bryta-idolen-6k@icloud.com>
2025-01-31 00:04:26 +0100

    w

---
+++ script.js
@@ -0,0 +1,6 @@
+const restart = () => {
+  const MARKUP = document.body.innerHTML;
+  document.body.innerHTML = '';
+  requestAnimationFrame(() => document.body.innerHTML = MARKUP);
+};
+document.body.addEventListener('click', restart);
